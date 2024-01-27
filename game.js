ed5af03d2e24db2b7f9c664eeab9d90ab5f93df7
Kat-Str <120758963+Kat-Str@users.noreply.github.com>
2024-01-27 19:02:26 +0100

    Integrate sound effects and sequence animation

--- game.js
+++ game.js
@@ -2,36 +2,67 @@ var gamePattern = [];
 var userClickedPattern = [];
 var buttonColors = ["red", "blue", "green", "yellow"];
 
+// Sounds
+var redAudio = new Audio("./sounds/red.mp3");
+var blueAudio = new Audio("./sounds/blue.mp3");
+var greenAudio = new Audio("./sounds/green.mp3");
+var yellowAudio = new Audio("./sounds/yellow.mp3");
+
+var failAudio = new Audio("./sounds/wrong.mp3");
+
 function nextSequence() {
   var randomNumber = Math.floor(Math.random() * 4);
   var randomChosenColor = buttonColors[randomNumber];
   gamePattern.push(randomChosenColor);
-}
+  new Audio(`./sounds/${randomChosenColor}.mp3`).play();
+  console.log(randomChosenColor);
 
-nextSequence();
+  // Animate sequence
+  if (randomChosenColor === "red") {
+    $("#red").addClass("pressed");
+    setTimeout(() => {
+      $("#red").removeClass("pressed");
+    }, 150);
+  } else if (randomChosenColor === "blue") {
+    $("#blue").addClass("pressed");
+    setTimeout(() => {
+      $("#blue").removeClass("pressed");
+    }, 150);
+  } else if (randomChosenColor === "green") {
+    $("#green").addClass("pressed");
+    setTimeout(() => {
+      $("#green").removeClass("pressed");
+    }, 150);
+  } else {
+    $("#yellow").addClass("pressed");
+    setTimeout(() => {
+      $("#yellow").removeClass("pressed");
+    }, 150);
+  }
+}
 
 $("#red").click(function () {
   var userChosenColor = $("#red").attr("id");
-  console.log(userChosenColor);
   userClickedPattern.push(userChosenColor);
+  redAudio.play();
   console.log(userClickedPattern);
 });
 $("#blue").click(function () {
   var userChosenColor = $("#blue").attr("id");
-  console.log(userChosenColor);
   userClickedPattern.push(userChosenColor);
+  blueAudio.play();
   console.log(userClickedPattern);
 });
 $("#green").click(function () {
   var userChosenColor = $("#green").attr("id");
-  console.log(userChosenColor);
   userClickedPattern.push(userChosenColor);
+  greenAudio.play();
   console.log(userClickedPattern);
 });
 $("#yellow").click(function () {
   var userChosenColor = $("#yellow").attr("id");
-  console.log(userChosenColor);
   userClickedPattern.push(userChosenColor);
+  yellowAudio.play();
   console.log(userClickedPattern);
 });
 
@@ -41,3 +72,5 @@ $(".btn").click(function () {
     $(this).removeClass("pressed");
   }, 150);
 });
+
+$(document).on("keydown", nextSequence);
